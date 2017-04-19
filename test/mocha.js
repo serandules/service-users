7408f0b4fda19ff459355894b027f99ee6fa32d5
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-04-19 20:21:46 +0530

    updating

--- test/mocha.js
+++ test/mocha.js
@@ -4,6 +4,6 @@ nconf.overrides({
     'services': [
         {"name": "service-configs", "version": "master", "domain": "accounts", "prefix": "/apis/v/configs"},
         {"name": "service-tokens", "version": "master", "domain": "accounts", "prefix": "/apis/v/tokens"},
-        {"service": require('../'), "domain": "accounts", "prefix": "/apis/v/users"}
+        {"path": __dirname + '/..', "domain": "accounts", "prefix": "/apis/v/users"}
     ]
 });
