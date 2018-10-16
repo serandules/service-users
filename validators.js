b88d6f7cc5d15784e1661cee51d4ea5e25d1d470
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-10-16 08:57:29 +0530

    updating

--- validators.js
+++ validators.js
@@ -3,26 +3,30 @@ var Users = require('model-users');
 var serandi = require('serandi');
 
 exports.find = function (req, res, next) {
-    validators.query(req, res, function (err) {
-        if (err) {
-            return next(err);
-        }
-        validators.find({
-            model: Users
-        }, req, res, next);
-    });
+  validators.query(req, res, function (err) {
+    if (err) {
+      return next(err);
+    }
+    validators.find({
+      model: Users
+    }, req, res, next);
+  });
 };
 
 exports.update = function (req, res, next) {
-    serandi.otp(req, res, function (err) {
-      if (err) {
-        return next(err);
-      }
-      validators.update({
-        id: req.params.id,
-        model: Users
-      }, req, res, next);
-    });
+  validators.update({
+    id: req.params.id,
+    model: Users
+  }, req, res, function (err) {
+    if (err) {
+      return next(err);
+    }
+    var data = req.body;
+    if (!data.password) {
+      return next();
+    }
+    serandi.otp(req, res, next);
+  });
 };
 
 exports.create = function (req, res, next) {
@@ -32,7 +36,7 @@ exports.create = function (req, res, next) {
   }, req, res, next);
 };
 
-exports.createk = function (req, res, next) {
+exports.createOld = function (req, res, next) {
   var data = req.body;
   var validator = validators.types.password({
     blocked: {
@@ -44,7 +48,7 @@ exports.createk = function (req, res, next) {
     value: data.password
   }, function (err) {
     if (err) {
-        console.log(err)
+      console.log(err)
       return next(err);
     }
     validators.create({
